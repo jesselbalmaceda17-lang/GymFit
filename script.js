6bc2c9aabe143ffc2b689b47c56ecde3adbdf060
jessel <jesselbalmaceda17@gmail.com>
2026-05-21 20:40:20 -0600

    Added Netlify Forms

--- script.js
+++ script.js
@@ -6,12 +6,3 @@ exploreBtn.addEventListener('click', () => {
     });
 });
 
-const form = document.querySelector('form');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-
-    alert('Message sent successfully!');
-
-    form.reset();
-});
